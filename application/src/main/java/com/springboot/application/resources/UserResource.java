69bcf04f9f3103c4a26d998cab93b077de9c8e07
Ivo Almeida <ijpsalmeida@gmail.com>
2024-07-25 20:56:04 +0100

    Add H2 database

--- application/src/main/java/com/springboot/application/resources/UserResource.java
+++ application/src/main/java/com/springboot/application/resources/UserResource.java
@@ -1,5 +1,8 @@
 package com.springboot.application.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,11 @@ import com.springboot.application.entities.User;
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Ivo", "test", "1234");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = new ArrayList<>();
+		userList.add(new User(1L, "Ivo", "test", "1234"));
+		userList.add(new User(2L, "Mafalda", "test", "1234"));
+
+		return ResponseEntity.ok().body(userList);
 	}
 }
